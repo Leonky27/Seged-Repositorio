9fa7956f99e258fd60f7d8fc1269821b516eb42e
Leonky27 <stevenpajaro27@gmail.com>
2025-09-29 22:52:59 -0500

    class usuario que guarda en mongo funcional

--- src/main/java/com/proyecto/seged/model/Usuario.java
+++ src/main/java/com/proyecto/seged/model/Usuario.java
@@ -1,4 +1,80 @@
 package com.proyecto.seged.model;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "usuarios")
 public class Usuario {
+
+    @Id
+    private ObjectId id;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String cedula;
+
+    private String celular;
+
+    private String email;
+
+    private Boolean activo;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
 }
