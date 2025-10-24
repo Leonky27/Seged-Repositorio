0f5594318fe3bad36e7852329f51c6644e094da9
Ronald <Ronaldiglesiasgonzalez@gmail.com>
2025-10-23 20:40:46 -0500

    dockerfile

--- project-front-main/project-front-main/src/main.jsx
+++ project-front-main/project-front-main/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './auth/AuthContext.jsx'
+import { AuthProvider } from './Auth/AuthContext.jsx'
 import { MainRoute } from './routes/MainRoute.jsx'
 
 createRoot(document.getElementById('root')).render(
