2f37efea51099974d472b2e75edfaac5c6cbfefc
Ronald <Ronaldiglesiasgonzalez@gmail.com>
2025-11-17 20:04:50 -0500

    dashboard

--- project-front-main/project-front-main/src/admin/Dashboard.jsx
+++ project-front-main/project-front-main/src/admin/Dashboard.jsx
@@ -2,7 +2,7 @@
 export const Dashboard = () => {
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh" }}>
-      <div style={{ width: "90vw", height: "90vh", maxWidth: "1200px", maxHeight: "800px" }}><iframe title="DASHBOARD" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiODc2YTQ0YmMtOTU4ZS00NTk4LTkxMDAtYjEzNTgyZjVmMjg1IiwidCI6IjlkMTJiZjNmLWU0ZjYtNDdhYi05MTJmLTFhMmYwZmM0OGFhNCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe></div>
+      <div style={{ width: "90vw", height: "90vh", maxWidth: "1200px", maxHeight: "800px" }}><iframe title="DASBOARD" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMTRkM2I0NmMtMjg1OS00MzY3LTgyZTItZTk4MjgyMTdiODI2IiwidCI6IjlkMTJiZjNmLWU0ZjYtNDdhYi05MTJmLTFhMmYwZmM0OGFhNCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe></div>
     </div>
   )
 }
